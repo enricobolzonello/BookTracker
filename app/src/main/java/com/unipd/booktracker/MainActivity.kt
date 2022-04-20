b12dfec12d9ebfa1d4ccfb8ee76ca1e4991cbfcd
Daniele Moschetta <76683899+danielemoschetta@users.noreply.github.com>
2022-04-20 14:08:45 +0200

    main

--- app/src/main/java/com/unipd/booktracker/MainActivity.kt
+++ app/src/main/java/com/unipd/booktracker/MainActivity.kt
@@ -2,10 +2,28 @@ package com.unipd.booktracker
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.navigation.findNavController
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.setupActionBarWithNavController
+import androidx.navigation.ui.setupWithNavController
+import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val navView: BottomNavigationView = findViewById(R.id.nav_view)
+
+        val navController = findNavController(R.id.nav_host_fragment)
+        // Passing each menu ID as a set of Ids because each
+        // menu should be considered as top level destinations.
+        val appBarConfiguration = AppBarConfiguration(
+            setOf(
+                R.id.navigation_library, R.id.navigation_wishlist, R.id.navigation_stats
+            )
+        )
+        setupActionBarWithNavController(navController, appBarConfiguration)
+        navView.setupWithNavController(navController)
     }
 }
