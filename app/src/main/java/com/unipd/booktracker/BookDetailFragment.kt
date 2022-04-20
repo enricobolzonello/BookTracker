67b733d606f69327262fc9ed6fe1d57b16edbc16
Daniele Moschetta <76683899+danielemoschetta@users.noreply.github.com>
2022-04-20 20:28:00 +0200

    action bar with search added

---
+++ app/src/main/java/com/unipd/booktracker/BookDetailFragment.kt
@@ -0,0 +1,18 @@
+package com.unipd.booktracker
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+
+class BookDetailFragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_book_detail, container, false)
+    }
+}
--- app/src/main/java/com/unipd/booktracker/MainActivity.kt
+++ app/src/main/java/com/unipd/booktracker/MainActivity.kt
@@ -1,7 +1,11 @@
 package com.unipd.booktracker
 
+import android.app.SearchManager
+import android.content.Context
 import android.os.Bundle
+import android.view.Menu
 import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.widget.SearchView
 import androidx.navigation.fragment.NavHostFragment
 import androidx.navigation.ui.NavigationUI
 import com.google.android.material.bottomnavigation.BottomNavigationView
@@ -14,6 +18,17 @@ class MainActivity : AppCompatActivity() {
 
         val navHostFragment = supportFragmentManager.findFragmentById(R.id.nav_host_fragment) as NavHostFragment
         val bottomNavigationView = findViewById<BottomNavigationView>(R.id.nav_view)
-        NavigationUI.setupWithNavController(bottomNavigationView, navHostFragment.navController);
+        NavigationUI.setupWithNavController(bottomNavigationView, navHostFragment.navController)
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu): Boolean {
+        menuInflater.inflate(R.menu.action_bar_menu, menu)
+
+        // Associate searchable configuration with the SearchView
+        val searchManager = getSystemService(Context.SEARCH_SERVICE) as SearchManager
+        (menu.findItem(R.id.search).actionView as SearchView).apply {
+            setSearchableInfo(searchManager.getSearchableInfo(componentName))
+        }
+        return true
     }
 }
