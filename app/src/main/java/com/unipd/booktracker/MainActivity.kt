7c06354f3dc85c415b96d6950fb13da34f16fd2e
Daniele Moschetta <76683899+danielemoschetta@users.noreply.github.com>
2022-04-21 15:44:06 +0200

    main

--- app/src/main/java/com/unipd/booktracker/MainActivity.kt
+++ app/src/main/java/com/unipd/booktracker/MainActivity.kt
@@ -6,27 +6,32 @@ import android.os.Bundle
 import android.view.Menu
 import androidx.appcompat.app.AppCompatActivity
 import androidx.appcompat.widget.SearchView
+import androidx.navigation.NavController
 import androidx.navigation.fragment.NavHostFragment
 import androidx.navigation.ui.NavigationUI
 import com.google.android.material.bottomnavigation.BottomNavigationView
 
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var navController : NavController
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
         val navHostFragment = supportFragmentManager.findFragmentById(R.id.nav_host_fragment) as NavHostFragment
+        navController = navHostFragment.navController
+
         val bottomNavigationView = findViewById<BottomNavigationView>(R.id.nav_view)
-        NavigationUI.setupWithNavController(bottomNavigationView, navHostFragment.navController)
+        NavigationUI.setupWithNavController(bottomNavigationView, navController)
     }
 
     override fun onCreateOptionsMenu(menu: Menu): Boolean {
         menuInflater.inflate(R.menu.action_bar_menu, menu)
-
         // Associate searchable configuration with the SearchView
         val searchManager = getSystemService(Context.SEARCH_SERVICE) as SearchManager
-        (menu.findItem(R.id.search).actionView as SearchView).apply {
+        val searchView = menu.findItem(R.id.search_view).actionView as SearchView
+        searchView.apply {
             setSearchableInfo(searchManager.getSearchableInfo(componentName))
         }
         return true
--- app/src/main/java/com/unipd/booktracker/SettingsFragment.kt
+++ app/src/main/java/com/unipd/booktracker/SettingsFragment.kt
@@ -1,10 +1,8 @@
 package com.unipd.booktracker
 
 import android.os.Bundle
+import android.view.*
 import androidx.fragment.app.Fragment
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
 
 class SettingsFragment : Fragment() {
 
