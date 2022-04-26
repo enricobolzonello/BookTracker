40963b78bde4110d3d95162a868b55bc5306bdc3
Daniele Moschetta <76683899+danielemoschetta@users.noreply.github.com>
2022-04-26 18:24:09 +0200

    MainActivity binding

--- app/src/main/java/com/unipd/booktracker/MainActivity.kt
+++ app/src/main/java/com/unipd/booktracker/MainActivity.kt
@@ -4,44 +4,38 @@ import android.os.Bundle
 import android.view.View
 import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
-import androidx.navigation.NavController
 import androidx.navigation.fragment.NavHostFragment
 import androidx.navigation.ui.NavigationUI
-import androidx.recyclerview.widget.RecyclerView
-import com.google.android.material.bottomnavigation.BottomNavigationView
-import com.google.android.material.floatingactionbutton.ExtendedFloatingActionButton
+import com.unipd.booktracker.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
     private val viewModel : BookViewModel by viewModels()
-    private lateinit var navController : NavController
+    private lateinit var binding : ActivityMainBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
 
-        val navHostFragment =
-            supportFragmentManager.findFragmentById(R.id.nav_host_fragment) as NavHostFragment
-        navController = navHostFragment.navController
-
-        val bottomNavigationView = findViewById<BottomNavigationView>(R.id.nav_view)
-        NavigationUI.setupWithNavController(bottomNavigationView, navController)
+        val navController = (binding.navHostFragment.getFragment() as NavHostFragment).navController
+        NavigationUI.setupWithNavController(binding.navView, navController)
 
         navController.addOnDestinationChangedListener { _, destination, _ ->
             when(destination.id) {
                 R.id.navigation_settings -> {
                     supportActionBar?.setDisplayHomeAsUpEnabled(true)
                     supportActionBar?.setTitle(R.string.title_settings)
-                    bottomNavigationView.visibility = View.GONE
+                    binding.navView.visibility = View.GONE
                 }
                 R.id.navigation_book_detail -> {
                     supportActionBar?.setDisplayHomeAsUpEnabled(true)
                     supportActionBar?.setTitle(R.string.title_bookdetail)
-                    bottomNavigationView.visibility = View.GONE
+                    binding.navView.visibility = View.GONE
                 }
                 else -> {
                     supportActionBar?.setDisplayHomeAsUpEnabled(false)
                     supportActionBar?.setTitle(R.string.app_name)
-                    bottomNavigationView.visibility = View.VISIBLE
+                    binding.navView.visibility = View.VISIBLE
                 }
             }
         }
