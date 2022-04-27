f70f5a629fa8f566d6113b447297f7e3048bd24f
Daniele Moschetta <76683899+danielemoschetta@users.noreply.github.com>
2022-04-27 19:27:53 +0200

    fixed settings fragment

--- app/src/main/java/com/unipd/booktracker/fragments/SettingsFragment.kt
+++ app/src/main/java/com/unipd/booktracker/fragments/SettingsFragment.kt
@@ -11,22 +11,8 @@ import com.unipd.booktracker.databinding.FragmentSettingsBinding
 
 class SettingsFragment : PreferenceFragmentCompat() {
 
-    // https://developer.android.com/guide/topics/ui/settings <-- guida per la pagina delle impostazioni
-    private lateinit var binding: FragmentSettingsBinding
-
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-
-        binding = FragmentSettingsBinding.inflate(layoutInflater)
-    }
-
-    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View {
-        // Inflate the layout for this fragment
-        binding = FragmentSettingsBinding.inflate(inflater, container, false)
-        return binding.root
-    }
-
     override fun onCreatePreferences(savedInstanceState: Bundle?, rootKey: String?) {
         setPreferencesFromResource(R.xml.preference_settings, rootKey)
     }
+
 }
