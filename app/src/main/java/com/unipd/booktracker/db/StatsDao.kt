ee8f3ee5bdc4a8bc88b483755a18fa0d00b92498
Daniele Moschetta <76683899+danielemoschetta@users.noreply.github.com>
2022-05-09 16:25:45 +0200

    page stats count only positive changes

--- app/src/main/java/com/unipd/booktracker/db/StatsDao.kt
+++ app/src/main/java/com/unipd/booktracker/db/StatsDao.kt
@@ -8,10 +8,10 @@ interface StatsDao {
     @Query("SELECT SUM(readPages) FROM books")
     fun countReadPages(): Int
 
-    @Query("SELECT SUM(pageDifference) FROM readings WHERE date = :day")
+    @Query("SELECT SUM(pageDifference) FROM readings WHERE date = :day AND pageDifference > 0")
     fun countReadPages(day: String): Int
 
-    @Query("SELECT AVG(pageDifference) FROM readings WHERE date < :day GROUP BY strftime('%Y-%m-%d', date)")
+    @Query("SELECT AVG(pageDifference) FROM readings WHERE date < :day AND pageDifference > 0 GROUP BY strftime('%Y-%m-%d', date)")
     fun avgReadPagesByDay(day: String): Int
 
     @Query("SELECT COUNT(*) FROM books WHERE readPages = pages")
@@ -28,16 +28,16 @@ interface StatsDao {
                         "(SELECT strftime('%Y', date) AS year FROM readings WHERE books.id = bookId AND year < :year ORDER BY date DESC LIMIT 1))")
     fun avgReadBooksByYear(year: String): Int
 
-    @Query("SELECT mainAuthor FROM books GROUP BY mainAuthor ORDER BY COUNT(*) DESC LIMIT 1")
+    @Query("SELECT mainAuthor FROM books WHERE readPages IS NOT NULL GROUP BY mainAuthor ORDER BY COUNT(*) DESC LIMIT 1")
     fun mostReadAuthor(): String?
 
-    @Query("SELECT COUNT(DISTINCT mainAuthor) FROM books")
+    @Query("SELECT COUNT(DISTINCT mainAuthor) FROM books WHERE readPages IS NOT NULL")
     fun countAuthors(): Int
 
-    @Query("SELECT mainCategory FROM books GROUP BY mainCategory ORDER BY COUNT(*) DESC LIMIT 1")
+    @Query("SELECT mainCategory FROM books  WHERE readPages IS NOT NULL GROUP BY mainCategory ORDER BY COUNT(*) DESC LIMIT 1")
     fun mostReadCategory(): String?
 
-    @Query("SELECT COUNT(DISTINCT mainCategory) FROM books")
+    @Query("SELECT COUNT(DISTINCT mainCategory) FROM books WHERE readPages IS NOT NULL")
     fun countCategories(): Int
 
 }
--- app/src/main/java/com/unipd/booktracker/fragments/StatsFragment.kt
+++ app/src/main/java/com/unipd/booktracker/fragments/StatsFragment.kt
@@ -30,7 +30,6 @@ class StatsFragment : Fragment() {
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
-        // Inflate the layout for this fragment
         _binding = FragmentStatsBinding.inflate(inflater, container, false)
         return binding.root
     }
